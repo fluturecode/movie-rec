03ade8474b05f8e131fcb3fa02b82136eafc9ad0
Fluturecode <fluturecode@gmail.com>
2024-07-23 22:51:40 -0400

    added  more detail to get_movie_recommendations fx

--- recommendation_system.py
+++ recommendation_system.py
@@ -19,16 +19,32 @@ item_similarity = cosine_similarity(user_item_matrix.T)
 
 # Function to get movie recommendations
 def get_movie_recommendations(movie_name, similarity_matrix, movie_titles):
-    # Find the index of the movie
-    idx = movie_titles[movie_titles == movie_name].index[0]
+    """
+    Get movie recommendations based on the provided movie name.
+    
+    Parameters:
+    movie_name (str): The name of the movie for which to find recommendations.
+    similarity_matrix (numpy.ndarray): The similarity matrix where each element [i][j] 
+    represents the similarity score between movie i and movie j.
+    movie_titles (pandas.Series): A pandas Series containing movie titles indexed by movieId.
+    
+    Returns:
+    pandas.Series: A series of the top 10 most similar movie titles.
+    """
+    try:
+        # Find the index of the movie in the movie_titles series
+        idx = movie_titles[movie_titles == movie_name].index[0]
+    except IndexError:
+        # If the movie is not found, return an empty series
+        return pd.Series([], name="No recommendations found")
     
     # Get the similarity scores for all movies with that movie
     sim_scores = list(enumerate(similarity_matrix[idx]))
     
-    # Sort the movies based on the similarity scores
+    # Sort the movies based on the similarity scores in descending order
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
     
-    # Get the scores of the 10 most similar movies
+    # Get the scores of the 10 most similar movies (excluding the first one, which is the movie itself)
     sim_scores = sim_scores[1:11]
     
     # Get the movie indices
@@ -41,6 +57,7 @@ def get_movie_recommendations(movie_name, similarity_matrix, movie_titles):
 movie_titles = data['title'].drop_duplicates()
 
 # Example usage
-recommendations = get_movie_recommendations('Toy Story (1995)', item_similarity, movie_titles)
+movie_titles = data['title'].drop_duplicates()
+recommendations = get_movie_recommendations('Home Alone (1990)', item_similarity, movie_titles)
 print("Recommended movies for 'Toy Story (1995)':")
 print(recommendations)
